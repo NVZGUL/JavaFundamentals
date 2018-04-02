52738b9a22d9158d6ae98fd6530afc8f21365b29
NVZGUL <h3rmess@gmail.com>
2018-04-02 19:14:07 +0300

    refactored to 1 method for better perfomance

--- 03-strings/src/main/java/task1/CrazyLogger.java
+++ 03-strings/src/main/java/task1/CrazyLogger.java
@@ -8,19 +8,14 @@ import java.time.format.DateTimeFormatter;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public final class CrazyLogger {
-    final StringBuilder sb = new StringBuilder();
-    final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY:hh-mm");
+    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY:hh-mm");
     private CrazyLogger(){
     }
-    private boolean addMsg(String msg){
-        sb.append(LocalDateTime.now().format(formatter));
-        sb.append(String.format("-%s", msg));
-        return true;
-    }
     public static void INFO(String str){
-        CrazyLogger cl = new CrazyLogger();
-        if (cl.addMsg(str)){
-            System.out.println(cl.sb.toString());
-        }
+        String res = new StringBuilder()
+                .append(LocalDateTime.now().format(formatter))
+                .append(String.format("-%s", str))
+                .toString();
+        System.out.println(res);
     }
 }
